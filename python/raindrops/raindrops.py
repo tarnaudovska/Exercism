55738bdbe1cb92f9dbcf3957f9fb8cb6baa66de7
Tamara Arnaudovska <tdanevska@gmail.com>
2025-04-02 13:36:15 +0200

    exercism Implement raindrop sound conversion based on number divisibility

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,14 @@
+def convert(number):
+    raindrop = ""
+
+    if number%3==0:
+        raindrop += "Pling"
+    if number%5==0:
+        raindrop += "Plang"
+    if number%7==0:
+        raindrop += "Plong"
+
+    if not raindrop:
+        return str(number)
+    
+    return raindrop
