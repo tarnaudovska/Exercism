81494026b808a25e2047c8856ece099989c92b62
Tamara Arnaudovska <tdanevska@gmail.com>
2025-06-09 12:56:11 +0200

    exercism isogram second sollution

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,4 +1,12 @@
 def is_isogram(string):
     string = string.lower()
-    letters = [char for char in string if char.isalpha()]
-    return len(letters) == len(set(letters))
+    letters = []
+    for char in string:
+        if char.isalpha():
+            letters.append(char)
+    unique_letters = set(letters)
+
+    if len(letters) == len(unique_letters):
+        return True
+    else:
+        return False
